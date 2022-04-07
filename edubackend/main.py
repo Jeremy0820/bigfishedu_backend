500423ec8cfbd3ca43114e83b8ff5bba1931c66e
于建财 <yujiancai1@xiaomi.com>
2022-04-07 10:00:58 +0800

    add module path

--- edubackend/main.py
+++ edubackend/main.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, BASE_DIR)
+
+
 from edubackend import create_flask_app
 from flask_cors import CORS
 from common.settings.default import DefaultConfig
